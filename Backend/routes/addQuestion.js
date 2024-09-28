bf28d37f7f5b1cfdb32a9807cb31cff9974361ab
Ceilingsz <chaitanyanemmani@gmail.com>
2024-09-28 11:05:04 +0530

    delete extra files

--- Backend/routes/addQuestion.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verifyAdmin = require('../middlewares/verifyAdmin');
-const addQuestion = require('../controllers/addQuestionController');
-
-router.post('/addQuestion', verifyAdmin, addQuestion);
-
-module.exports = router;
